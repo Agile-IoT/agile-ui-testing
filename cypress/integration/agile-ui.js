661df7ee1b0becf8c73d0debaf74d144e1f2909b
Eduard Brehm <eduard.brehm>
2018-06-26 17:10:16 +0200

    Update config and add test to add, update and delete attribute

--- cypress/integration/agile-ui.js
+++ cypress/integration/agile-ui.js
@@ -31,19 +31,43 @@ function expand() {
   })
 }
 
-describe('User view policies', function() {
+describe('User view privileges', () => {
   beforeEach(() => {
     authAndVisitUI()
     cy.wait(3000) //Let react load all state objects
   })
 
-  it('Switch to user overview tab and view first user attributes', function() {
-    cy.get('div[value="/"]').get('button').then(tabs => {
-      tabs[4].click()
+  it('Add and update attribute to own user entity and delete it', () => {
+    cy.get('#navigation').get('button').then(tabs => {
+      var workingtab = conf.tabs.profile
+      tabs[workingtab.index].click()
+      cy.url().should('contain', '/entity/').and('match', /\/user/)
+      expand()
+      cy.get('#cypress').should('not.exist')
+      cy.get('span').contains('cypress').should('not.exist')
+      cy.get('span').contains('cypress.value').should('not.exist')
+      cy.get('#new-attribute').type('cypress')
+      cy.get('#new-attribute-value').type('cypress.value')
+      cy.get('#new-attribute-submit').click()
+      cy.get('#cypress').should('exist')
+      cy.get('#cypress_value').should('have.value', 'cypress.value')
+      cy.get('#cypress_value').clear()
+      cy.get('#cypress_value').type('updated.value')
+      cy.get('#navigation')
+      cy.get('#cypress_value').should('have.value', 'updated.value')
+      cy.get('#delete_cypress').click()
+      cy.get('#cypress').should('not.exist')
+    })
+  })
+
+  it('Switch to user overview tab and view first user attributes', () => {
+    cy.get('#navigation').get('button').then(tabs => {
+      var workingtab = conf.tabs.user
+      tabs[workingtab.index].click()
       cy.location('pathname').should('have', '/list/user')
       cy.wait(1500)
       cy.get('.container--app').get('a').then(viewbuttons => {
-        viewbuttons[0].click() //First user attributes
+        viewbuttons[workingtab.buttons.view].click() //First user attributes
         cy.url().should('contain', '/entity/').and('match', /\/user/)
         cy.wait(1500)
         cy.get('#new_password').should('exist')
@@ -54,13 +78,14 @@ describe('User view policies', function() {
     })
   })
 
-  it('Switch to user overview tab and view second user attributes', function() {
-    cy.get('div[value="/"]').get('button').then(tabs => {
-      tabs[4].click()
+  it('Switch to user overview tab and view second user attributes', () => {
+    cy.get('#navigation').get('button').then(tabs => {
+      var workingtab = conf.tabs.user
+      tabs[workingtab.index].click()
       cy.location('pathname').should('have', '/list/user')
       cy.wait(1500)
       cy.get('.container--app').get('a').then(viewbuttons => {
-        viewbuttons[3].click() //Second user attributes
+        viewbuttons[workingtab.buttons.view + Object.keys(workingtab.buttons).length].click() //Second user attributes
         cy.url().should('contain', '/entity/').and('match', /\/user/)
         cy.wait(1500)
         cy.get('#new_password').should('exist')
