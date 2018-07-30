2b5b43f4dfd52af335f911c0f07ec1f93f001678
Eduard Brehm <eduard.brehm>
2018-07-30 15:47:12 +0200

    Use general configuration file and add second user to use different policy evaluation within one iteration

--- cypress/integration/agile-wordpress-heavy.js
+++ cypress/integration/agile-wordpress-heavy.js
@@ -1,17 +1,25 @@
-const wordpressConf = {
-  "host": "192.168.1.106",
-  "adminuser": "agile",
-  "adminpassword": "secret"
-}
+let wordpressConf = require('../../conf.json')
 
-describe('Heavy WordPress test', () => {
-  it('Login 100 times and logout', () => {
+describe('WordPress test', () => {
+  it('Login 100 times with an admin user and a normal user and logout', () => {
     for (let i = 0; i < 100; i++) {
       cy.visit(wordpressConf.host)
       cy.visit(wordpressConf.host + '/wp-admin')
       cy.get('#user_login').type(wordpressConf.adminuser)
-      cy.get('#user_pass').type(wordpressConf.adminpassword)
+      cy.get('#user_pass').type(wordpressConf.adminusersecret)
       cy.get('#wp-submit').click()
+      cy.get('#wp-admin-bar-logout').children().then(elems => {
+        cy.wrap(elems).click({force: true})
+        cy.url().should('be', wordpressConf.host + '/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2F&reauth=1')
+      })
+      cy.visit(wordpressConf.host)
+      cy.visit(wordpressConf.host + '/wp-admin')
+      cy.get('#user_login').type(wordpressConf.user)
+      cy.get('#user_pass').type(wordpressConf.usersecret)
+      cy.get('#wp-submit').click()
+      cy.url().should('be', wordpressConf.host + '/wp-admin/profile.php')
+      cy.get('body').should('contain', 'Sorry, you are not allowed to edit this user.')
+      cy.visit(wordpressConf.host + '/wp-admin')
       cy.get('#wp-admin-bar-logout').children().then(elems => {
         cy.wrap(elems).click({force: true})
         cy.url().should('be', wordpressConf.host + '/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2F&reauth=1')
