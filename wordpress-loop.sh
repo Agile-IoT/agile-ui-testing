a493adeb5953da8c3de394ac922ed4b6c6ffd1a9
dp <dp>
2018-10-07 18:34:17 +0200

    update script to cleanup start and kill performance-monitoring

--- wordpress-loop.sh
+++ wordpress-loop.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 cp results-template.json results.json 
+rm -r /tmp/perf*
+HERE=`pwd`
+cd ../performance-monitor/
+node src/index.js  > /dev/null 2>&1 &
+PRO=$! 
+echo "process $PRO"
+cd $HERE
 npm run wordpress
 while [ $? -eq 1 ]
 do 
 	npm run wordpress
 done
+kill -INT $PRO
+
