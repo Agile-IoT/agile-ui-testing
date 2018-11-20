70853b95e3a15f645a24deabb2e7165b55591db5
dp <dp>
2018-11-20 17:12:40 +0100

    fix more async issues and remove waits

--- cypress/integration/agile-ui-security.js
+++ cypress/integration/agile-ui-security.js
@@ -78,62 +78,73 @@ describe('Security User', () => {
     let tabs = undefined
     cy.get('#navigation').get('button').then(t => {
       tabs = t
-      tabs[conf.tabs.groups.index].click()
+      return tabs[conf.tabs.groups.index].click()
+    }).then(()=>{
       return cy.get('#cypress-agile-local').should('not.exist')
     }).then(( )=> {
       //Create group
       return cy.get('#navigation').get('button')
     }).then(t => {
       tabs = t
-      tabs[conf.tabs.groups.index].click()
+      return tabs[conf.tabs.groups.index].click()
+    }).then( ()=>{
       let regex = new RegExp(conf.tabs.groups.path.replace(/:[a-zA-Z]*/, '.*'))
-      cy.url().should('match', regex)
-      cy.wait(500)
-      cy.get('#new_entity_button').click()
+      return cy.url().should('match', regex)
+    }).then(()=>{
+      return cy.get('#new_entity_button').click()
+    }).then(()=>{
       cy.get('#root_group_name').type('cypress-group')
-      cy.get('button[type="submit"]').click()
-      cy.wait(500)
+      return cy.get('button[type="submit"]').click()
+    }).then(()=>{
       return cy.get('#navigation').get('button')
     }).then(t => {
       tabs = t
       //Assign group
-      tabs[conf.tabs.userlist.index].click()
-      cy.location('pathname').should('eq', conf.tabs.userlist.path)
-      cy.get('#group_' + conf.adminuser + '-agile-local').click()
-      cy.get('#root_groups').should('exist')
-      cy.get('#root_groups').contains('cypress-group').find(':checkbox').should('be.not.checked')
-      cy.get('#root_groups').contains('cypress-group').find(':checkbox').check()
-      return cy.get('button[type="submit"]')
-    }).then( (submit) => {
-      cy.log('sumbit button')
-
-        cy.log(submit)
-        submit.click()
-        cy.wait(3000)
-        tabs[conf.tabs.groups.index].click()
-        cy.get('#view_cypress-group-' + conf.user + '-agile-local').click()
+      return tabs[conf.tabs.userlist.index].click()
+    }).then(()=>{
+      return cy.location('pathname').should('eq', conf.tabs.userlist.path)
+    }).then(()=>{
+      return cy.get('#group_' + conf.adminuser + '-agile-local').click()
+    }).then(()=>{
+      return cy.get('#root_groups').should('exist')
+    }).then(()=>{
+      return cy.get('#root_groups').contains('cypress-group').find(':checkbox').should('be.not.checked')
+    }).then(()=>{
+      return cy.get('#root_groups').contains('cypress-group').find(':checkbox').check()
+    }).then(()=>{
+      return cy.get('button[type="submit"]').click()
+    }).then(()=>{
+        return tabs[conf.tabs.groups.index].click()
+    }).then(()=>{
+        return cy.get('#view_cypress-group-' + conf.user + '-agile-local').click()
+      }).then(()=>{
         expand(true)
-        cy.wait(1000)
-        cy.get('#root-entities').should('contain', 'agile!@!agile-local')
+        cy.wait(4000)
+        return cy.get('#root-entities').should('contain', 'agile!@!agile-local')
+      }).then(()=>{
         //Remove user from group
         return cy.get('#navigation').get('button')
-
       }).then(t => {
         tabs = t
         tabs[conf.tabs.userlist.index].click()
-        cy.location('pathname').should('eq', conf.tabs.userlist.path)
-        cy.get('#group_' + conf.adminuser + '-agile-local').click()
-        cy.wait(1000)
-        cy.get('#root_groups').should('exist')
-        cy.get('#root_groups').contains('cypress-group').find(':checkbox').should('be.checked')
-        cy.get('#root_groups').contains('cypress-group').find(':checkbox').uncheck()
+        return cy.location('pathname').should('eq', conf.tabs.userlist.path)
+      }).then(()=>{
+        return cy.get('#group_' + conf.adminuser + '-agile-local').click()
+      }).then(()=>{
+        return cy.get('#root_groups').should('exist')
+      }).then(()=>{
+        return cy.get('#root_groups').contains('cypress-group').find(':checkbox').should('be.checked')
+      }).then(()=>{
+        return cy.get('#root_groups').contains('cypress-group').find(':checkbox').uncheck()
+      }).then(()=>{
         return cy.get('button[type="submit"]')
 
       }).then(submit => {
-        submit.click()
-        cy.wait(3000)
+        return submit.click()
+      }).then(()=>{
         tabs[conf.tabs.groups.index].click()
-        cy.get('#view_cypress-group-' + conf.user + '-agile-local').click()
+        return cy.get('#view_cypress-group-' + conf.user + '-agile-local').click()
+      }).then(()=>{
         expand(true)
         cy.get('#root-entities').should('not.contain', conf.adminuser + '!@!agile-local')
       }).then(() =>{
@@ -141,35 +152,135 @@ describe('Security User', () => {
         cy.get('#navigation').get('button').then(tabs => {
         tabs[conf.tabs.groups.index].click()
         cy.get('#cypress-group-' + conf.user + '-agile-local').should('exist')
-        cy.get('#delete_cypress-group-' + conf.user + '-agile-local').click()
-        cy.wait(1000)
+        return cy.get('#delete_cypress-group-' + conf.user + '-agile-local').click()
+      }).then(()=>{
         cy.get('#cypress-group-' + conf.user + '-agile-local').should('not.exist')
     })
   })
   })
   it('Create and delete entities', () => {
+    let workingtab = undefined
     cy.get('#navigation').get('button').then(tabs => {
-      tabs[conf.tabs.userlist.index].click()
-      cy.get('#cypress-agile-local').should('not.exist')
-      return cy.get('#navigation').get('button')
+      return tabs[conf.tabs.userlist.index].click()
+    }).then(()=>{
+      return cy.get('#cypress-agile-local').should('not.exist')
+    }).then(()=>{
+          return cy.get('#navigation').get('button')
     }).then(tabs => {
       tabs[conf.tabs.userlist.index].click()
       let regex = new RegExp(conf.tabs.userlist.path.replace(/:[a-zA-Z]*/, '.*'))
-      cy.url().should('match', regex)
-      cy.wait(1500)
-      cy.get('#new_entity_button').click()
-      cy.location('pathname').should('eq', conf.views.addUser.path)
-      cy.get('#root_user_name').type('cypress')
-      cy.get('#root_auth_type').select('agile-local')
-      cy.get('#root_password').type('secret')
-      cy.get('#root_role').select('admin')
-      cy.get('button[type="submit"]').click()
-      cy.wait(3000)
+      return cy.url().should('match', regex)
+    }).then(()=>{
+      return cy.get('#new_entity_button').click()
+    }).then(()=>{
+      return cy.location('pathname').should('eq', conf.views.addUser.path)
+    }).then(()=>{
+      return cy.get('#root_user_name').type('cypress')
+    }).then(()=>{
+
+      return cy.get('#root_auth_type').select('agile-local')
+    }).then(()=>{
+
+      return cy.get('#root_password').type('secret')
+    }).then(()=>{
+
+      return cy.get('#root_role').select('admin')
+    }).then(()=>{
+
+      return cy.get('button[type="submit"]').click()
+    }).then(()=>{
       return cy.get('#navigation').get('button')
     }).then(tabs => {
-      let workingtab = conf.tabs.userlist
-      tabs[workingtab.index].click()
+       workingtab = conf.tabs.userlist
+      return tabs[workingtab.index].click()
+    }).then(()=>{
       cy.get('#cypress-agile-local').should('not.exist')
     })
   })
+
+  it('Add and update attribute to own user entity and delete it', () => {
+    let workingtab = undefined
+    cy.get('#navigation').get('button').then(tabs => {
+      workingtab = conf.tabs.profile
+      tabs[workingtab.index].click()
+      let regex = new RegExp(workingtab.path.replace(/:[a-zA-Z]*/, '.*'))
+      cy.url().should('match', regex)
+      expand()
+      cy.wait(4000)
+      return cy.get('#cypress').should('not.exist')
+    }).then(()=>{
+      return cy.get('span').contains('cypress').should('not.exist')
+    }).then(()=>{
+      return cy.get('span').contains('cypress.value').should('not.exist')
+    }).then(()=>{
+      return cy.get('#new-attribute').type('cypress')
+    }).then(()=>{
+      return cy.get('#new-attribute-value').type('cypress.value')
+    }).then(()=>{
+      return cy.get('#new-attribute-submit').click()
+    }).then(()=>{
+      return cy.get('#cypress').should('exist')
+    }).then(()=>{
+      return cy.get('#cypress_value').should('have.value', 'cypress.value')
+    }).then(()=>{
+      return cy.get('#cypress_value').clear()
+    }).then(()=>{
+      return cy.get('#cypress_value').type('updated.value')
+    }).then(()=>{
+      return cy.get('#navigation')
+    }).then(()=>{
+      return cy.get('#cypress_value').should('have.value', 'updated.value')
+    }).then(()=>{
+      return cy.get('#delete_cypress').click()
+    }).then(()=>{
+      cy.get('#cypress').should('not.exist')
+    })
+  })
+  it('Switch to user overview tab and view first user attributes', () => {
+   let workingtab = undefined
+   cy.get('#navigation').get('button').then(tabs => {
+     workingtab = conf.tabs.userlist
+     return tabs[workingtab.index].click()
+   }).then(()=>{
+     return cy.location('pathname').should('eq', workingtab.path)
+   }).then(()=>{
+    return cy.get('#view_' + conf.adminuser + '-agile-local').click() //First user attributes
+  }).then(()=>{
+     let regex = new RegExp(conf.views.user.path.replace(/:[a-zA-Z]*/, '.*'))
+     return cy.url().should('match', regex)
+    }).then(()=>{
+     return cy.get('#' + conf.adminuser + '-agile-local')
+    }).then(()=>{
+     return cy.get('#new_password').should('exist')
+   }).then(()=>{
+     return cy.get('#old_password').should('not.exist')
+    }).then(()=>{
+     expand()
+     cy.wait(4000)
+     cy.get('span').contains('id').should('be.visible')
+   })
+ })
+
+ it('Switch to user overview tab and view second user attributes', () => {
+   let workingtab = undefined
+   cy.get('#navigation').get('button').then(tabs => {
+     workingtab = conf.tabs.userlist
+     return tabs[workingtab.index].click()
+   }).then(()=>{
+     return cy.location('pathname').should('eq', workingtab.path)
+   }).then(()=>{
+     return cy.get('#view_' + conf.user + '-agile-local').click() //First user attributes
+   }).then(()=>{
+      let regex = new RegExp(conf.views.user.path.replace(/:[a-zA-Z]*/, '.*'))
+     return cy.url().should('match', regex)
+   }).then(()=>{
+     return cy.get('#new_password').should('exist')
+     }).then(()=>{
+     return cy.get('#old_password').should('exist')
+   }).then(()=>{
+      expand()
+     cy.get('span').contains('id').should('be.visible')
+   })
+ })
+
 })
